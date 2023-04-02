e482ae8a5bde4f1137238d60782500924e19793f
Your Name <you@example.com>
2023-04-01 23:58:47 -0500

    bd

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class contactos(models.Model):
+    contactos = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.contactos
--- demo/settings.py
+++ demo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'AAm5nEOtTxWp7uYUppAI',
+        'HOST': 'containers-us-west-120.railway.app',
+        'PORT': '7221'
     }
 }
 
